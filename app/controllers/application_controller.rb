f989ab7523a7a29385d8cd24effd23968ccea6bd
James Vanderburg <james@vanderburg.org>
2023-11-14 20:14:12 -0600

    Implement logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,9 @@ class ApplicationController < ActionController::Base
     reset_session
     session[:user_id] = user.id
   end
+
+  def logout(user)
+    Current.user = nil
+    reset_session
+  end
 end
